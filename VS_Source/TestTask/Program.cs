6424f8562e5927e74a19858bf8df2c21ac610b6b
Alexander <alex20010804@gmail.com>
2023-08-30 19:38:42 +0300

    setup project
    1st setup project

---
+++ VS_Source/TestTask/Program.cs
@@ -0,0 +1,14 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace TestTask.Startup
+{
+    public class Program
+    {
+        //"server=127.0.0.1;port=3306;username=root;password=;database=test_task"
+
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
